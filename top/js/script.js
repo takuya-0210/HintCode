86aea1dac291c6e5e7a64b46b9db674e64e2aaf7
takuya <uver0210tata@gmai.com>
2024-03-12 20:50:17 +0900

    css-article cleate

--- top/js/script.js
+++ top/js/script.js
@@ -62,11 +62,10 @@ function TextTypingAnime() {
   
   $('.TextTyping').each(function() {
     
-    let elemPos = $(this).offset().top,
+    let elemPos = $(this).offset().top + 300,
         scroll = $(window).scrollTop(),
         windowHeight = $(window).height(),
         thisChild = "";
-    
     if(scroll >= elemPos - windowHeight) {
       thisChild = $(this).children();
       thisChild.delay(600).each(function(i) {
@@ -125,14 +124,25 @@ const swiper = new Swiper(".swiper", {
 
 
 
-const Html_searchItems = document.querySelectorAll('.search-btn');
+
+
+
+//section-search
+const searchItems = document.querySelectorAll('.search-btn');
 const Html_searchItem1_list = document.querySelector('#Html-item1-list');
 const Html_List1Item = document.querySelector('#Html-item1-ListItem');
 const Html_searchItem2_list = document.querySelector('#Html-item2-list');
 const Html_List2Item = document.querySelector('#Html-item2-ListItem');
 
+const Css_searchItem1_list = document.querySelector('#Css-item1-list');
+const Css_List1Item = document.querySelector('#Css-item1-ListItem');
+const Css_searchItem2_list = document.querySelector('#Css-item2-list');
+const Css_List2Item = document.querySelector('#Css-item2-ListItem');
 
-Html_searchItems[0].addEventListener('click', () => {
+
+//Html-searchBtn = 0,1
+//Css-searchBtn = 2,3
+searchItems[0].addEventListener('click', () => {
   
   Html_searchItem1_list.classList.toggle('active');
   if (Html_searchItem2_list.classList.contains('active') === true ) {
@@ -146,7 +156,7 @@ Html_searchItems[0].addEventListener('click', () => {
 
 });
 
-Html_searchItems[1].addEventListener('click', () => {
+searchItems[1].addEventListener('click', () => {
   
   Html_searchItem2_list.classList.toggle('active');
   if (Html_searchItem1_list.classList.contains('active') === true ) {
@@ -159,13 +169,41 @@ Html_searchItems[1].addEventListener('click', () => {
   }
 });
 
+searchItems[2].addEventListener('click', () => {
+  
+  Css_searchItem1_list.classList.toggle('active');
+  if (Css_searchItem2_list.classList.contains('active') === true ) {
+    Css_searchItem2_list.classList.remove('active');
+  }
+
+  Css_List1Item.classList.toggle('on');
+  if(Css_List2Item.classList.contains('on') === true) {
+    Css_List2Item.classList.remove('on');
+  }
+});
+
+searchItems[3].addEventListener('click', () => {
+  
+  Css_searchItem2_list.classList.toggle('active');
+  if (Css_searchItem1_list.classList.contains('active') === true ) {
+    Css_searchItem1_list.classList.remove('active');
+  }
+
+  Css_List2Item.classList.toggle('on');
+  if(Css_List1Item.classList.contains('on') === true) {
+    Css_List1Item.classList.remove('on');
+  }
+});
 
 
 
 
-const item1_categories = document.querySelectorAll('#Html-item1-list > ul > li');
 
-item1_categories.forEach(function (element) {
+
+//Html-要素欄-カテゴリー毎の呼び出し
+const H_item1_categories = document.querySelectorAll('#Html-item1-list > ul > li');
+
+H_item1_categories.forEach(function (element) {
 
   function reset() {
     const productList = document.querySelectorAll('#Html-item1-ListItem > ul > li');
@@ -177,7 +215,7 @@ item1_categories.forEach(function (element) {
   element.addEventListener('click', function() {
     switch (element) {
 
-      case item1_categories[0]: {
+      case H_item1_categories[0]: {
         reset();
         const Item_all = document.querySelectorAll('#Html-item1-ListItem > ul > .all');
         for( let j of Item_all) {
@@ -186,7 +224,7 @@ item1_categories.forEach(function (element) {
       }
       break;
     
-        case item1_categories[1]:{
+        case H_item1_categories[1]:{
           reset();
           const Item_no1 = document.querySelectorAll('#Html-item1-ListItem > ul > .no1');
           for( let j of Item_no1) {
@@ -195,7 +233,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[2]:{
+        case H_item1_categories[2]:{
           const Item_no2 = document.querySelectorAll('#Html-item1-ListItem > ul > .no2');
           reset();
           for( let j of Item_no2) {
@@ -204,7 +242,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[3]:{
+        case H_item1_categories[3]:{
           const Item_no3 = document.querySelectorAll('#Html-item1-ListItem > ul > .no3');
           reset();
           for( let j of Item_no3) {
@@ -213,7 +251,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[4]:{
+        case H_item1_categories[4]:{
           const Item_no4 = document.querySelectorAll('#Html-item1-ListItem > ul > .no4');
           reset();
           for( let j of Item_no4) {
@@ -222,7 +260,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[5]:{
+        case H_item1_categories[5]:{
           const Item_no5 = document.querySelectorAll('#Html-item1-ListItem > ul > .no5');
           reset();
           for( let j of Item_no5) {
@@ -231,7 +269,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[6]:{
+        case H_item1_categories[6]:{
           const Item_no6 = document.querySelectorAll('#Html-item1-ListItem > ul > .no6');
           reset();
           for( let j of Item_no6) {
@@ -240,7 +278,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[7]:{
+        case H_item1_categories[7]:{
           const Item_no7 = document.querySelectorAll('#Html-item1-ListItem > ul > .no7');
           reset();
           for( let j of Item_no7) {
@@ -249,7 +287,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[8]:{
+        case H_item1_categories[8]:{
           const Item_no8 = document.querySelectorAll('#Html-item1-ListItem > ul > .no8');
           reset();
           for( let j of Item_no8) {
@@ -258,7 +296,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[9]:{
+        case H_item1_categories[9]:{
           const Item_no9 = document.querySelectorAll('#Html-item1-ListItem > ul > .no9');
           reset();
           for( let j of Item_no9) {
@@ -267,7 +305,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[10]:{
+        case H_item1_categories[10]:{
           const Item_no10 = document.querySelectorAll('#Html-item1-ListItem > ul > .no10');
           reset();
           for( let j of Item_no10) {
@@ -276,7 +314,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[11]:{
+        case H_item1_categories[11]:{
           const Item_no11 = document.querySelectorAll('#Html-item1-ListItem > ul > .no11');
           reset();
           for( let j of Item_no11) {
@@ -285,7 +323,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[12]:{
+        case H_item1_categories[12]:{
           const Item_no12 = document.querySelectorAll('#Html-item1-ListItem > ul > .no12');
           reset();
           for( let j of Item_no12) {
@@ -294,7 +332,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[13]:{
+        case H_item1_categories[13]:{
           const Item_no13 = document.querySelectorAll('#Html-item1-ListItem > ul > .no13');
           reset();
           for( let j of Item_no13) {
@@ -303,7 +341,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[14]:{
+        case H_item1_categories[14]:{
           const Item_no14 = document.querySelectorAll('#Html-item1-ListItem > ul > .no14');
           reset();
           for( let j of Item_no14) {
@@ -312,7 +350,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[15]:{
+        case H_item1_categories[15]:{
           const Item_no15 = document.querySelectorAll('#Html-item1-ListItem > ul > .no15');
           reset();
           for( let j of Item_no15) {
@@ -321,7 +359,7 @@ item1_categories.forEach(function (element) {
         }
         break;
 
-        case item1_categories[16]:{
+        case H_item1_categories[16]:{
           const Item_no16 = document.querySelectorAll('#Html-item1-ListItem > ul > .no16');
           reset();
           for( let j of Item_no16) {
@@ -331,7 +369,6 @@ item1_categories.forEach(function (element) {
         break;
     }
   });
-  
 });
 
 
@@ -344,12 +381,10 @@ item1_categories.forEach(function (element) {
 
 
 
+//Html-属性欄-カテゴリー毎の呼び出し
+const H_item2_categories = document.querySelectorAll('#Html-item2-list > ul > li');
 
-const item2_categories = document.querySelectorAll('#Html-item2-list > ul > li');
-
-
-
-item2_categories.forEach(function (element) {
+H_item2_categories.forEach(function (element) {
 
   function reset () {
     const productList = document.querySelectorAll('#Html-item2-ListItem > ul >li');
@@ -361,7 +396,7 @@ item2_categories.forEach(function (element) {
   element.addEventListener('click', function() {
     switch (element) {
 
-      case item2_categories[0]: {
+      case H_item2_categories[0]: {
         reset();
         const Item_all = document.querySelectorAll('#Html-item2-ListItem > ul > .all');
         for( let j of Item_all) {
@@ -370,7 +405,7 @@ item2_categories.forEach(function (element) {
       }
       break;
     
-      case item2_categories[1]:{
+      case H_item2_categories[1]:{
         reset();
         const Item_no1 = document.querySelectorAll('#Html-item2-ListItem > ul > .no1');
         for( let j of Item_no1) {
@@ -379,7 +414,7 @@ item2_categories.forEach(function (element) {
       }
         break;
 
-        case item2_categories[2]:{
+        case H_item2_categories[2]:{
           reset();
           const Item_no2 = document.querySelectorAll('#Html-item2-ListItem > ul > .no2');
           for( let j of Item_no2) {
@@ -399,8 +434,188 @@ item2_categories.forEach(function (element) {
 
 
 
+//Css-プロパティ欄-カテゴリー毎の呼び出し
+const C_item1_categories = document.querySelectorAll('#Css-item1-list > ul > li');
+
+C_item1_categories.forEach(function (element) {
+
+  function reset() {
+    const productList = document.querySelectorAll('#Css-item1-ListItem > ul > li');
+    for (let r of productList) {
+      r.style.display = 'none';
+    }
+  }
+
+  function productShow (value) {
+    for(let v of value) {
+      v.style.display = 'block';
+    }
+  }
+
+  element.addEventListener('click', function() {
+    switch (element) {
+
+      case C_item1_categories[0]: {
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .all'));
+      }
+      break;
+    
+      case C_item1_categories[1]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no1'));
+      }
+      break;
+
+      case C_item1_categories[2]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no2'));
+      }
+      break;
+
+      case C_item1_categories[3]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no3'));
+      }
+      break;
+
+      case C_item1_categories[4]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no4'));
+      }
+      break;
+
+      case C_item1_categories[5]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no5'));
+      }
+      break;
+
+      case C_item1_categories[6]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no6'));
+      }
+      break;
+
+      case C_item1_categories[7]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no7'));
+      }
+      break;
+
+      case C_item1_categories[8]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no8'));
+      }
+      break;
+
+      case C_item1_categories[9]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no9'));
+      }
+      break;
+
+      case C_item1_categories[10]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no10'));
+      }
+      break;
+
+      case C_item1_categories[11]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no11'));
+      }
+      break;
+
+      case C_item1_categories[12]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item1-ListItem > ul > .no12'));
+      }
+      break;
+    }
+  });
+});
 
 
 
 
 
+
+//Css-疑似クラス欄-カテゴリー毎の呼び出し
+const C_item2_categories = document.querySelectorAll('#Css-item2-list > ul > li');
+
+C_item2_categories.forEach(function (element) {
+
+  function reset () {
+    const productList = document.querySelectorAll('#Css-item2-ListItem > ul >li');
+    for (let r of productList) {
+      r.style.display = 'none';
+    }
+  }
+
+  function productShow (value) {
+    for(let v of value) {
+      v.style.display = 'block';
+    }
+  }
+
+  element.addEventListener('click', function() {
+    switch (element) {
+
+      case C_item2_categories[0]: {
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .all'));
+      }
+      break;
+    
+      case C_item2_categories[1]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no1'));
+      }
+      break;
+
+      case C_item2_categories[2]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no2'));
+      }
+      break;
+
+      case C_item2_categories[3]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no3'));
+      }
+      break;
+
+      case C_item2_categories[4]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no4'));
+      }
+      break;
+
+      case C_item2_categories[5]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no5'));
+      }
+      break;
+
+      case C_item2_categories[6]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no6'));
+      }
+      break;
+
+      case C_item2_categories[7]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no7'));
+      }
+      break;
+
+      case C_item2_categories[8]:{
+        reset();
+        productShow(document.querySelectorAll('#Css-item2-ListItem > ul > .no8'));
+      }
+      break;
+    }
+  });
+  
+});
