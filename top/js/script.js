a8246c5b411dbde076d93a6e0c4db91b03d5b910
takuya <uver0210tata@gmai.com>
2024-03-16 21:18:35 +0900

    footer cleate

--- top/js/script.js
+++ top/js/script.js
@@ -107,10 +107,130 @@ $(window).on('load', function(){
 
 
 
+//pagination-show
+window.addEventListener('scroll', () => {
+  const pageScroll = window.scrollY;
+  const target = document.querySelector('#pagination');
+  //console.log(pageScroll)
+  if (pageScroll > 300) {
+    target.style.position = 'fixed';
+    target.style.top = '0';
+    target.style.opacity = '1';
+    target.style.transitionDuration = '2s';
+  } else {
+    target.style.position = 'fixed';
+    target.style.top = '-150px';
+    target.style.opacity = '0';
+    target.style.transitionDuration = '2s';
+  }
+});
+
+
+//pagination-titleColor-change
+window.addEventListener('scroll', function () {
+  const scrollY = window.scrollY;
+  const targets = document.querySelectorAll('article');
+  const scrollTop = scrollY || document.documentElement.scrollTop;
+  const target1 = targets[1].getBoundingClientRect();
+  const target1Top = target1.top + scrollTop;
+  const target1Bottom = target1.bottom + scrollTop;
+  const target2 = targets[2].getBoundingClientRect();
+  const target2Top = target2.top + scrollTop;
+  const target2Bottom = target2.bottom + scrollTop;
+  const target3 = targets[3].getBoundingClientRect();
+  const target3Top = target3.top + scrollTop;
+  const target3Bottom = target3.bottom + scrollTop;
+  const marks = document.querySelectorAll('#pagination span');
+
+  if (scrollY > target1Top - 250) {
+    marks[1].classList.add('Hnow');
+  } else if (scrollY < target1Top) {
+    marks[1].classList.remove('Hnow');
+  }
+   if (scrollY > target1Bottom -100) {
+    marks[1].classList.remove('Hnow');
+  }
+
+  if (scrollY > target2Top - 250) {
+    marks[2].classList.add('Cnow');
+  } else if (scrollY < target2Top) {
+    marks[2].classList.remove('Cnow');
+  }
+   if (scrollY > target2Bottom -100) {
+    marks[2].classList.remove('Cnow');
+  }
+
+  if (scrollY > target3Top - 250) {
+    marks[3].classList.add('Jnow');
+  }  else if (scrollY < target3Top) {
+    marks[3].classList.remove('Jnow');
+  }
+   if (scrollY > target3Bottom -100) {
+    marks[3].classList.remove('Jnow');
+  }
+
+});
+
+
+
+
+//Smooth Scrolling
+window.addEventListener('DOMContentLoaded', () => {
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+ 
+  const homeLinks = document.querySelectorAll('#HomeArticle > li a[href^="#"]');
+  anchorLinksArr.forEach(link => {
+    link.addEventListener('click', e => {
+      console.log(anchorLinks)
+      e.preventDefault();
+      const targetId = link.hash;
+      const targetElement = document.querySelector(targetId);
+      const targetOffsetTop = window.scrollY + targetElement.getBoundingClientRect().top;
+      window.scrollTo ({
+        top: targetOffsetTop  - 250,
+        behavior: "smooth",
+      });
+    });
+  });
+
+  
+  const homeLinksArr = Array.prototype.slice.call(homeLinks);
+console.log()
+  homeLinksArr.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.hash;
+      const targetElement = document.querySelector(targetId);
+      const targetOffsetTop = window.scrollY + targetElement.getBoundingClientRect().top;
+      console.log('true')
+      window.scrollTo ({
+        top: targetOffsetTop - 500,
+        behavior: "smooth",
+      });
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*swiper*/
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiperSection .swiper", {
   slidesPerView: 3,
   slidesPerView: 1.5,
   spaceBetween: 100,
@@ -623,3 +743,59 @@ C_item2_categories.forEach(function (element) {
   });
   
 });
+
+
+
+
+
+const JsSwiper = new Swiper ('.summarize .swiper', {
+  loop: true,
+  loopAdditionalSlides: 1,
+  slidesPerView: 5,
+  spaceBetween: 10,
+  centeredSlides: true,
+  watchSlidesProgress: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+const footerSwiper = new Swiper ('#footerSection1 .swiper', {
+  loop: true,
+  loopSlides: 2,
+  slidesPerView: "3",
+  speed: 10000,
+  autoplay: {
+    delay: 0,
+    pauseOnMouseEnter: true, 
+    disableOnInteraction: false,
+  },
+  mousewheel: {
+    thresholdDelta: 70,
+    forceToAxis: true,
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
